6c7939cf8559cfe4f952c98ba56459cbc42c6a5e
Bicky <absinghd@gmail.com>
2019-07-09 14:12:26 -0700

    modernJavascript Course

---
+++ modernJavascriptCourse/exercises/sandbox.js
@@ -0,0 +1,17 @@
+let age = 25;
+let year = 2019;
+
+//this is a comment
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+/* var score = 12;
+console.log(score);
+
+var score = 3;
+console.log(score); */
