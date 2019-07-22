f889d103a0280d3aaa5bb2222170777a964c3515
Bicky <absinghd@gmail.com>
2019-07-22 02:56:58 -0700

    reorganization

--- modernJavascriptCourse/exercises/sandbox.js
+++ modernJavascriptCourse/exercises/sandbox.js
@@ -1,5 +1,23 @@
 const scores = [10, 30, 15, 25, 50, 40, 5];
 
-console.log(scores.filter((score) => {
-    return score > 20;
-}));
+// const filteredScores = scores.filter((score) => {
+//     return score > 20;
+// });
+
+// console.log(scores);
+// console.log(filteredScores);
+
+const users = [
+    {name: 'shaun', premium: true},
+    {name: 'yoshi', premium: false},
+    {name: 'mario', premium: false},
+    {name: 'chun-li', premium: true}
+  ];
+
+  const premiumUsers = users.filter((user) => {
+      return user.premium;
+  });
+
+  console.log(premiumUsers);
+
+  
