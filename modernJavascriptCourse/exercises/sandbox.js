e8c4daf8368b278ed2d7f4500b5a03dd112be9c7
Bicky <absinghd@gmail.com>
2019-07-24 15:03:57 -0700

    added

--- modernJavascriptCourse/exercises/sandbox.js
+++ modernJavascriptCourse/exercises/sandbox.js
@@ -1,23 +1,5 @@
-const scores = [10, 30, 15, 25, 50, 40, 5];
+const scores = [10, 20, 60, 40, 70, 90, 30];
 
-// const filteredScores = scores.filter((score) => {
-//     return score > 20;
-// });
-
-// console.log(scores);
-// console.log(filteredScores);
-
-const users = [
-    {name: 'shaun', premium: true},
-    {name: 'yoshi', premium: false},
-    {name: 'mario', premium: false},
-    {name: 'chun-li', premium: true}
-  ];
-
-  const premiumUsers = users.filter((user) => {
-      return user.premium;
-  });
-
-  console.log(premiumUsers);
-
-  
+const results = scores.reduce((acc, curr) => {
+    
+})
