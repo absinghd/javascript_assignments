5c42784c940163475f7d2c6a15a4f8fd6d94c427
Bicky <absinghd@gmail.com>
2019-07-26 15:49:38 -0700

    changes in reduce function

--- modernJavascriptCourse/exercises/sandbox.js
+++ modernJavascriptCourse/exercises/sandbox.js
@@ -1,5 +1,26 @@
-const scores = [10, 20, 60, 40, 70, 90, 30];
+// const scores = [10, 20, 60, 40, 70, 90, 30];
 
-const results = scores.reduce((acc, curr) => {
-    
-})
+// const results = scores.reduce((acc, curr) => {
+//     if(curr > 50){
+//         acc++;
+//     }
+//     return acc;
+// }, 0);
+
+// console.log(results);
+
+const scores = [
+    {player: 'mario', score: 50},
+    {player: 'yoshi', score: 30},
+    {player: 'mario', score: 70},
+    {player: 'crystal', score: 60}
+  ]; 
+
+  const marioTotal = scores.reduce((acc, curr) => {
+      if (curr.player === 'mario'){
+          acc += curr.score;
+      }
+      return acc;
+  }, 0);
+
+  console.log(marioTotal);
