c0cab848b700bbba16413043fe9313168742c948
Bicky <absinghd@gmail.com>
2019-07-27 21:04:12 -0700

    changes in filter function

--- modernJavascriptCourse/exercises/sandbox.js
+++ modernJavascriptCourse/exercises/sandbox.js
@@ -1,26 +1,10 @@
-// const scores = [10, 20, 60, 40, 70, 90, 30];
+const products = [
+    {name: 'gold star', price: 30},
+    {name: 'green shell', price: 10},
+    {name: 'red shell', price: 40},
+    {name: 'banana skin', price: 5},
+    {name: 'mushroom', price: 50}
+  ];
 
-// const results = scores.reduce((acc, curr) => {
-//     if(curr > 50){
-//         acc++;
-//     }
-//     return acc;
-// }, 0);
-
-// console.log(results);
-
-const scores = [
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'crystal', score: 60}
-  ]; 
-
-  const marioTotal = scores.reduce((acc, curr) => {
-      if (curr.player === 'mario'){
-          acc += curr.score;
-      }
-      return acc;
-  }, 0);
-
-  console.log(marioTotal);
+  const filtered = products.filter(product => product.price >20);
+  
