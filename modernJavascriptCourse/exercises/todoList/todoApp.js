6fb84eb32f0a11eea72ac75b9f73699d527dc517
Bicky <absinghd@gmail.com>
2019-08-02 00:06:26 -0700

    can add and delete on to-do list

--- modernJavascriptCourse/exercises/todoList/todoApp.js
+++ modernJavascriptCourse/exercises/todoList/todoApp.js
@@ -1,15 +1,18 @@
 const addForm = document.querySelector('.add');
-const list = document.querySelector('.todos')
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
+
+//adding new to-dos
 const generateTemplate = todo => {
 
     const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
     <span>${todo}</span>
     <i class="far fa-trash-alt delete"></i>
-  </li>
-    `;
+  </li>`;
 
+    list.innerHTML += html;
 }
 
 addForm.addEventListener('submit', e => {
@@ -17,6 +20,32 @@ addForm.addEventListener('submit', e => {
     e.preventDefault();
     const todo = addForm.add.value.trim();
 
-    generateTemplate(todo);
-    console.log(todo);
-})
+    if(todo.length) {
+        generateTemplate(todo);
+        addForm.reset()
+    }
+});
+
+
+//delete to-dos
+
+list.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
+//search keyup
+
+const filterTodos = (term) => {
+    (Array.from(list.children))
+    .filter((todo) => {
+        return !todo.textContent.includes(term);
+    });
+};
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim();
+    filterTodos(term);
+
+});
