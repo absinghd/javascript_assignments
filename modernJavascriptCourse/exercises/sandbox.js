372a8a38d5aab018db854245a0440dd52a353f82
Bicky <absinghd@gmail.com>
2019-08-21 23:39:30 -0700

    promises

--- modernJavascriptCourse/exercises/sandbox.js
+++ modernJavascriptCourse/exercises/sandbox.js
@@ -1,28 +1,69 @@
-const getTodos = (callback) => {
+// const getTodos = (resource, callback) => {
 
-  const request = new XMLHttpRequest();
+//   const request = new XMLHttpRequest();
 
-  request.addEventListener('readystatechange', () => {
+//   request.addEventListener('readystatechange', () => {
 
-    if(request.readyState === 4 && request.status === 200){
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4){
-      callback('could not fetch the data', undefined);
-    }
+//     if(request.readyState === 4 && request.status === 200){
+//       const data = JSON.parse(request.responseText);
+//       callback(undefined, data);
+//     } else if (request.readyState === 4){
+//       callback('could not fetch the data', undefined);
+//     }
 
-  });
+//   });
   
-  request.open('GET', 'todos.json');
-  request.send();
+//   request.open('GET', resource);
+//   request.send();
+
+// };
+
+
+// const getSomething = () => {
+
+//   return new Promise((resolve, reject) => {
+
+//     resolve('some data');
+//     //reject('some error');
+//   });
+// }
+
+
+const getTodos = (resource) => {
+
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+  
+      if(request.readyState === 4 && request.status === 200){
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4){
+        reject('could not fetch the data');
+      }
+  
+    });
+    
+    request.open('GET', resource);
+    request.send();
+  });
 
 };
 
-getTodos((err, data) => {
-  console.log('callback function fired');
-  if(err){
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+getTodos('json/luigi.json').then(data => {
+  console.log('promise resolved:', data);
+}).catch(err => {
+  console.log('promise rejected:', err);
 });
+
+// promise example
+const getSomething = () => {
+
+  return new Promise((resolve, reject) => {
+    // do something (fetch data)
+    // resolve('some data');
+    reject('some error');
+  });
+
+};
