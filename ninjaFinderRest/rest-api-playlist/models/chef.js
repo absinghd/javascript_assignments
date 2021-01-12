7b07f446123de5bee96b8c531b017c1324ab0d6e
Bicky Singh <9771641+absinghd@users.noreply.github.com>
2021-01-12 13:45:17 -0800

    putting stop on api work with geo-schema

--- ninjaFinderRest/rest-api-playlist/models/chef.js
+++ ninjaFinderRest/rest-api-playlist/models/chef.js
@@ -1,5 +1,25 @@
+
+//youtube course for ninja: https://www.youtube.com/watch?v=k8mi38BI55g&list=PL4cUxeGkcC9jBcybHMTIia56aV21o2cZ8&index=15
+//also look into using firebase geo-point
+
+
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+
+
+//create geolocation SChema
+const GeoSchema = new Schema({
+    type:{
+        type: String,
+        default: "Point"
+    },
+    coordinates:{
+        type: [Number],
+        index:"2dsphere"
+    }
+})
 
 //create chef Schema & model
 const ChefSchema = new Schema({
@@ -13,8 +33,9 @@ const ChefSchema = new Schema({
     available:{
         type:Boolean,
         default: false
-    }
-    //add in geo location
+    },
+    geometry: GeoSchema
+
 })
 
 const Chef = mongoose.model('chef', ChefSchema);
